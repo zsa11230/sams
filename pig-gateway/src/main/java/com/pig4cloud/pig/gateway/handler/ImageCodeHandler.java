df16000f637e3642c69a14270c4d0b2bed78050b
阮晨光 <117815667@qq.com>
2019-03-29 11:36:43 +0800

    修改验证码样式

--- pig-gateway/src/main/java/com/pig4cloud/pig/gateway/handler/ImageCodeHandler.java
+++ pig-gateway/src/main/java/com/pig4cloud/pig/gateway/handler/ImageCodeHandler.java
@@ -1,17 +1,5 @@
 /*
- *  Copyright (c) 2019-2020, 冷冷 (wangiegie@gmail.com).
- *  <p>
- *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  <p>
- * https://www.gnu.org/licenses/lgpl.html
- *  <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Copyright (c) 2019-2020, rcg .
  */
 
 package com.pig4cloud.pig.gateway.handler;
@@ -33,8 +21,13 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -52,8 +45,7 @@ public class ImageCodeHandler implements HandlerFunction<ServerResponse> {
 	@Override
 	public Mono<ServerResponse> handle(ServerRequest serverRequest) {
 		//生成验证码
-		String text = producer.createText();
-		BufferedImage image = producer.createImage(text);
+		String text = generateVerifyCode(4);
 
 		//保存验证码信息
 		String randomStr = serverRequest.queryParam("randomStr").get();
@@ -62,7 +54,8 @@ public class ImageCodeHandler implements HandlerFunction<ServerResponse> {
 		// 转换流信息写出
 		FastByteArrayOutputStream os = new FastByteArrayOutputStream();
 		try {
-			ImageIO.write(image, "jpeg", os);
+			int w = 102, h = 40;
+			outputImage(w, h, os, text);
 		} catch (IOException e) {
 			log.error("ImageIO write err", e);
 			return Mono.error(e);
@@ -73,4 +66,233 @@ public class ImageCodeHandler implements HandlerFunction<ServerResponse> {
 			.contentType(MediaType.IMAGE_JPEG)
 			.body(BodyInserters.fromResource(new ByteArrayResource(os.toByteArray())));
 	}
+
+
+	//使用到Algerian字体，系统里没有的话需要安装字体，字体只显示大写，去掉了1,0,i,o几个容易混淆的字符
+	public static final String VERIFY_CODES = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
+	private static Random random = new Random();
+
+
+	/**
+	 * 验证码对象
+	 * @author rcg
+	 *
+	 */
+	public static class Verify{
+
+		private String code;//如 1 + 2
+
+		private Integer value;//如  3
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public Integer getValue() {
+			return value;
+		}
+		public void setValue(Integer value) {
+			this.value = value;
+		}
+	}
+
+	/**
+	 * 使用系统默认字符源生成验证码
+	 * @return
+	 */
+	public static Verify generateVerify(){
+		int number1 = new Random().nextInt(10) + 1;;
+		int number2 = new Random().nextInt(10) + 1;;
+		Verify entity = new Verify();
+		entity.setCode(number1  + " x " + number2);
+		entity.setValue(number1 + number2);
+		return entity;
+	}
+
+	/**
+	 * 使用系统默认字符源生成验证码
+	 * @param verifySize    验证码长度
+	 * @return
+	 */
+	public static String generateVerifyCode(int verifySize){
+		return generateVerifyCode(verifySize, VERIFY_CODES);
+	}
+
+
+	/**
+	 * 使用指定源生成验证码
+	 * @param verifySize    验证码长度
+	 * @param sources   验证码字符源
+	 * @return
+	 */
+	public static String generateVerifyCode(int verifySize, String sources){
+		if(sources == null || sources.length() == 0){
+			sources = VERIFY_CODES;
+		}
+		int codesLen = sources.length();
+		Random rand = new Random(System.currentTimeMillis());
+		StringBuilder verifyCode = new StringBuilder(verifySize);
+		for(int i = 0; i < verifySize; i++){
+			verifyCode.append(sources.charAt(rand.nextInt(codesLen-1)));
+		}
+		return verifyCode.toString();
+	}
+
+
+	/**
+	 * 输出指定验证码图片流
+	 * @param w
+	 * @param h
+	 * @param os
+	 * @param code
+	 * @throws IOException
+	 */
+	public static void outputImage(int w, int h, OutputStream os, String code) throws IOException{
+		int verifySize = code.length();
+		BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		Random rand = new Random();
+		Graphics2D g2 = image.createGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		Color[] colors = new Color[5];
+		Color[] colorSpaces = new Color[] { Color.WHITE, Color.CYAN,
+			Color.GRAY, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE,
+			Color.PINK, Color.YELLOW };
+		float[] fractions = new float[colors.length];
+		for(int i = 0; i < colors.length; i++){
+			colors[i] = colorSpaces[rand.nextInt(colorSpaces.length)];
+			fractions[i] = rand.nextFloat();
+		}
+		Arrays.sort(fractions);
+
+		g2.setColor(Color.GRAY);// 设置边框色
+		g2.fillRect(0, 0, w, h);
+
+		Color c = getRandColor(200, 250);
+		g2.setColor(c);// 设置背景色
+		g2.fillRect(0, 2, w, h-4);
+
+		//绘制干扰线
+		Random random = new Random();
+		g2.setColor(getRandColor(160, 200));// 设置线条的颜色
+		for (int i = 0; i < 20; i++) {
+			int x = random.nextInt(w - 1);
+			int y = random.nextInt(h - 1);
+			int xl = random.nextInt(6) + 1;
+			int yl = random.nextInt(12) + 1;
+			g2.drawLine(x, y, x + xl + 40, y + yl + 20);
+		}
+
+		// 添加噪点
+		float yawpRate = 0.05f;// 噪声率
+		int area = (int) (yawpRate * w * h);
+		for (int i = 0; i < area; i++) {
+			int x = random.nextInt(w);
+			int y = random.nextInt(h);
+			int rgb = getRandomIntColor();
+			image.setRGB(x, y, rgb);
+		}
+
+		shear(g2, w, h, c);// 使图片扭曲
+
+		g2.setColor(getRandColor(100, 160));
+		int fontSize = h-4;
+		Font font = new Font("Algerian", Font.ITALIC, fontSize);
+		g2.setFont(font);
+		char[] chars = code.toCharArray();
+		for(int i = 0; i < verifySize; i++){
+			AffineTransform affine = new AffineTransform();
+			affine.setToRotation(Math.PI / 4 * rand.nextDouble() * (rand.nextBoolean() ? 1 : -1), (w / verifySize) * i + fontSize/2, h/2);
+			g2.setTransform(affine);
+			g2.drawChars(chars, i, 1, ((w-10) / verifySize) * i + 5, h/2 + fontSize/2 - 10);
+		}
+
+		g2.dispose();
+		ImageIO.write(image, "jpg", os);
+	}
+
+	private static Color getRandColor(int fc, int bc) {
+		if (fc > 255){
+			fc = 255;
+		}
+
+		if (bc > 255){
+			bc = 255;
+		}
+
+		int r = fc + random.nextInt(bc - fc);
+		int g = fc + random.nextInt(bc - fc);
+		int b = fc + random.nextInt(bc - fc);
+		return new Color(r, g, b);
+	}
+
+	private static int getRandomIntColor() {
+		int[] rgb = getRandomRgb();
+		int color = 0;
+		for (int c : rgb) {
+			color = color << 8;
+			color = color | c;
+		}
+		return color;
+	}
+
+	private static int[] getRandomRgb() {
+		int[] rgb = new int[3];
+		for (int i = 0; i < 3; i++) {
+			rgb[i] = random.nextInt(255);
+		}
+		return rgb;
+	}
+
+	private static void shear(Graphics g, int w1, int h1, Color color) {
+		shearX(g, w1, h1, color);
+		shearY(g, w1, h1, color);
+	}
+
+	private static void shearX(Graphics g, int w1, int h1, Color color) {
+
+		int period = random.nextInt(2);
+
+		boolean borderGap = true;
+		int frames = 1;
+		int phase = random.nextInt(2);
+
+		for (int i = 0; i < h1; i++) {
+			double d = (double) (period >> 1)
+				* Math.sin((double) i / (double) period
+				+ (6.2831853071795862D * (double) phase)
+				/ (double) frames);
+			g.copyArea(0, i, w1, 1, (int) d, 0);
+			if (borderGap) {
+				g.setColor(color);
+				g.drawLine((int) d, i, 0, i);
+				g.drawLine((int) d + w1, i, w1, i);
+			}
+		}
+
+	}
+
+	private static void shearY(Graphics g, int w1, int h1, Color color) {
+
+		int period = random.nextInt(40) + 10; // 50;
+
+		boolean borderGap = true;
+		int frames = 20;
+		int phase = 7;
+		for (int i = 0; i < w1; i++) {
+			double d = (double) (period >> 1)
+				* Math.sin((double) i / (double) period
+				+ (6.2831853071795862D * (double) phase)
+				/ (double) frames);
+			g.copyArea(i, 0, 1, h1, 0, (int) d);
+			if (borderGap) {
+				g.setColor(color);
+				g.drawLine(i, (int) d, i, 0);
+				g.drawLine(i, (int) d + h1, i, h1);
+			}
+
+		}
+
+	}
+
 }
